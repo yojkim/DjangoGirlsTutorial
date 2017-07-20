59a83136b823d80ed18d8b78a29caa4a0c48ce74
yojkim <youthful2016@gmail.com>
2017-07-20 23:54:15 +0900

    add draft function to general posting feature

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -37,3 +36,7 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
